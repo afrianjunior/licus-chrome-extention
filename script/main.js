674e1b1530bcccb907a9b60e5cd88384246db7ca
afrianjunior <afrian.junior26@gmail.com>
2017-11-11 22:32:18 +0700

    add: login function

--- script/main.js
+++ script/main.js
@@ -1,3 +1,7 @@
+const Canva = (c, self) => {
+
+}
+
 const Opening = (c, self) => {
   if (self.login.process === 'none') {
     return c("div", {attrs: {class: "opening"}}, [
@@ -13,7 +17,7 @@ const Opening = (c, self) => {
       }, self.home.btn)
     ])
   } else if (self.login.process === 'toLogin') {
-    return c("div", {attrs: {class: "opening"}}, [
+    return c("div", {attrs: {class: "opening open-login"}}, [
       c("div", {attrs: {class: "tag-top up"}}, self.home.tagOne),
       c("div", {attrs: {class: "tag-bottom up"}}, self.home.tagTwo),
       c("input", {
@@ -35,24 +39,84 @@ new Vue({
         tagTwo: 'Broh!',
         btn: 'Login'
       },
+      data: {
+        first: true,
+        login: false,
+        settings: {
+          password: '1234'
+        },
+        todos: [
+          {
+            title: 'hello'
+          }
+        ]
+      },
       login: {
         process: 'none'
-      }
+      },
+      tabs: null
     }
   },
   render (c) {
     if (this.page === 'home') {
-      return c("div", {}, [
+      return c("div", {attrs: {class: "canva"}}, [
+        Opening(c, this)
+      ])
+    } else if (this.page === 'toLogin') {
+      return c("div", {attrs: {class: "canva gradi"}}, [
         Opening(c, this)
       ])
     }
   },
   mounted () {
-    
+    var self = this;
+    this.initChange();
+    this.checkLogin();
   },
   methods: {
+    checkLogin() {
+      var self = this;
+      chrome.cookies.get({
+        url : 'http://junior.dev',
+        name: 'login'
+      }, (res) => {
+        if (res !== null) {
+          if (res.value == "false") {
+            self.data.login = false
+            console.log('yas')
+          } else if (res.value == "true") {
+            self.data.login = true
+          }
+        } else if (res === null) {
+          self.data.login = false
+          chrome.cookies.set({ 
+            url : 'http://junior.dev',
+            name: 'login',
+            value: "false"
+          }, () => {
+            console.log('init!')
+          })
+        }
+      })
+    },
+    initChange() {
+      chrome.cookies.onChanged.addListener((res) => {
+        if (res.cookie.domain === 'junior.dev') {
+          console.log(res)
+        }
+      });
+    },
     toLogin(val) {
+      var self = this;
+      this.page = 'toLogin'
       this.login.process = 'toLogin'
+      chrome.cookies.set({ 
+        url : 'http://junior.dev',
+        name: 'login',
+        value: JSON.stringify(self.data.login)
+      }, () => {
+        console.log('berhasil')
+      })
     }
   }
 }).$mount('#app');
